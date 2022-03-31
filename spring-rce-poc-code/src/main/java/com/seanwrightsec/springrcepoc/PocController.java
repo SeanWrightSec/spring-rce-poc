c75da98ad741d8b37de3735be7e4bb3e69315ad5
Sean Wright <sean@sean-wright.com>
2022-03-31 12:54:53 +0100

    Some further improvements

--- spring-rce-poc-code/src/main/java/com/seanwrightsec/springrcepoc/PocController.java
+++ spring-rce-poc-code/src/main/java/com/seanwrightsec/springrcepoc/PocController.java
@@ -1,7 +1,6 @@
 package com.seanwrightsec.springrcepoc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
